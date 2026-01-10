a5a07b1c0e13d6be6b560515defb7bb23604f35a
seb07uk <156347616+seb07uk@users.noreply.github.com>
2026-01-10 15:55:06 +0000

    add ascii art

---
+++ ASCII Art/Banner.bat
@@ -0,0 +1,28 @@
+@echo off
+chcp 65001 >nul
+setlocal ENABLEDELAYEDEXPANSION
+
+:: --- Kolory ---
+set "RED=[91m"
+set "GREEN=[92m"
+set "YELLOW=[93m"
+set "CYAN=[96m"
+set "RESET=[0m"
+
+:MENU
+cls
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m[0m 
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m[0m 
+echo.
+echo                      __           ______    _______________
+echo         ____  ____  / /________  / __/ /_  /  _/_  __/ ___/
+echo        / __ \/ __ \/ / ___/ __ \/ /_/ __/  / /  / /  \__ \ 
+echo       / /_/ / /_/ / (__  ) /_/ / __/ /__ _/ /  / /  ___/ / 
+echo      / .___/\____/_/____/\____/_/  \__(_)___/ /_/  /____/  
+echo     /_/          2026© Sebastian Januchowski                                          
+echo.
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m[0m            
+echo.[47m           [43m           [46m           [42m           [45m           [41m           [44m[0m 
+echo.
+pause
+
